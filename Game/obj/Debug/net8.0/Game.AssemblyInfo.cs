742c28800011de3678832d0c3242151bd94b8b7f
Brett Smith <brett03112@gmail.com>
2024-09-03 15:57:02 -0400

    updated with aider ai to be more functional

--- Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
+++ Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9530bd1e91f1d5173f2d7625cd6ea7aeb5bf87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c64381a99c61ba931cf2ba557963da8fe774ff1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
